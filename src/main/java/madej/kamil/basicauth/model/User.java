eb850950642f9a2f18657341c1afa6272c9ce4b4
KamilMad <kamilul085@gmail.com>
2024-01-21 00:53:26 +0100

    add user entity

---
+++ src/main/java/madej/kamil/basicauth/model/User.java
@@ -0,0 +1,27 @@
+package madej.kamil.basicauth.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "users")
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+}
